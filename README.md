001a99d862f73e91a6def62ef2f9c359f53fcb02
dev-aboali <dev.ahmed93@gmail.com>
2021-11-20 15:29:16 +0200

    completed landing page project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Landing Page Project
+
+## frontend nanno degree program udacity
+
+### Technologies
+
+-HTML5
+-CSS3
+-JavaScript (ES6)
+
+# Developed by: Ahmed Ali
---
+++ js/app.js
@@ -0,0 +1,118 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+let sections = document.querySelectorAll('section')
+let sectionList  = Array.from(sections)
+let navBarList = document.querySelector('#navbar__list')
+let menuLinks = document.getElementsByClassName('menu__link')
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+function buildNav() {
+    let fragment = new DocumentFragment()
+    sectionList.forEach(section => {
+        let li = document.createElement('li')
+        let a = document.createElement('a')
+        a.classList.add('menu__link')
+        a.href=`#${section.id}`
+        a.textContent = `${section.dataset.nav}`
+        li.appendChild(a)
+        fragment.appendChild(li)
+    })
+    navBarList.appendChild(fragment)    
+    
+}
+
+// Add class 'active' to section when near top of viewport
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            entry.target.classList.remove('your-active-class')
+            return;
+        }
+        // add active class to menu liks whenever we scroll to another section
+         Array.from(menuLinks).map((link) => {
+            let href = link.href.split('#')[1]
+            if(entry.target.id === href) {
+                link.classList.add('active')
+            } else {
+                link.classList.remove('active')
+            }
+        })
+        entry.target.classList.toggle('your-active-class')
+    })
+}, { threshold: 0.25, });
+
+// add your-active-class to observable section in the page
+function toggleActiveClass() {
+    
+    for (const section of sectionList) {
+        section.classList.remove('your-active-class')
+        observer.observe(section)
+    }
+
+}
+
+// Scroll to anchor ID using scrollTO event
+navBarList.addEventListener('click', (e) => {
+    e.preventDefault()
+    if(e.target.classList.contains('menu__link')) {
+        let href = e.target.href.split('#')[1]
+        let el = document.getElementById(href)
+        el.scrollIntoView({
+            behavior: "smooth"
+        }); 
+    }
+})
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+buildNav()
+// Scroll to section on link click
+
+
+// Set sections as active
+toggleActiveClass()
